9ca0213cdfaa1916d84a73a531b87fca39bb0632
Dominik Labuda <dominikla@gmail.com>
2019-04-29 00:11:09 +0200

    Initial java setup

---
+++ src/main/java/tk/vvtf/carorder/Main.java
@@ -0,0 +1,10 @@
+package tk.vvtf.carorder;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String... args) {
+        System.out.println(Arrays.toString(args));
+    }
+}
